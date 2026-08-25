3ae546c07371472d201e7a2de88a9a0f7535480d
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-25 13:01:10 +0800

    fix: apply model patch, add acceptance test

--- internal/store/snapshot.go
+++ internal/store/snapshot.go
@@ -15,6 +15,6 @@ func CloneAnodepitSnapshot(s model.PlantSnapshot) AnodepitSnapshotView {
 		Anodepit:     s.Anodepit,
 		Revision: s.Revision,
 	}
-	out.Alarms = s.Alarms[:len(s.Alarms):len(s.Alarms)]
+	out.Alarms = append([]model.AlarmEvent(nil), s.Alarms...)
 	return out
 }
