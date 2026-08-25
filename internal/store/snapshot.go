48135194b8ef8fa1986f0ca64a1db8e6d1efa94b
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-25 13:01:11 +0800

    red: acceptance test against unfixed code

---
+++ internal/store/snapshot.go
@@ -0,0 +1,20 @@
+package store
+
+import "github.com/lacsar712/anodbake/internal/model"
+
+type AnodepitSnapshotView struct {
+	UnitID   string
+	Anodepit     model.AnodepitReading
+	Alarms   []model.AlarmEvent
+	Revision uint64
+}
+
+func CloneAnodepitSnapshot(s model.PlantSnapshot) AnodepitSnapshotView {
+	out := AnodepitSnapshotView{
+		UnitID:   s.UnitID,
+		Anodepit:     s.Anodepit,
+		Revision: s.Revision,
+	}
+	out.Alarms = s.Alarms[:len(s.Alarms):len(s.Alarms)]
+	return out
+}
